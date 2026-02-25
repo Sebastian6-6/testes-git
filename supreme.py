022ee5a22eb24fe3e071b5ecaa2def7e52ef9a56
Sebastian6-6 <pedropiassi.business@gmail.com.com>
2026-02-25 12:08:15 -0300

    feat(data): adicionando script principal e notebook de testes (SCRUM-1)

--- supreme.py
+++ supreme.py
@@ -54,3 +54,15 @@ def verificacao_supreme():
     else: 
         print(f"\nVocê escolheu errado: {escolhas_nomes} \nTente outra vez!")
         return False
+
+
+def calcular_fatorial(n):
+    if n < 0:
+        return "Erro: Fatorial não existe para números negativos."
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        resultado = 1
+        for i in range(2, n + 1):
+            resultado *= i
+        return resultado
