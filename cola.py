9799b39b7d56ada77e2ec5ecc7c397846f2de425
Sebastian6-6 <pedropiassi.business@gmail.com>
2026-03-02 17:29:57 -0300

    feat: deploy automatico do cola.py (SCRUM-1) #comment Realizando o primeiro CD para o AWS Glue via GitHub Actions.

---
+++ cola.py
@@ -0,0 +1,18 @@
+import sys
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+print("Teste de Integração TDW: Script 'cola.py' executado com sucesso via GitHub Actions!")
+
+job.commit()
